4a61f200b3f6b909363c738a412af6a417bdfdba
masashiabe <tech.abe.jp@gmail.com>
2020-11-20 23:43:10 +0900

    create model

--- blogapp/posts/views.py
+++ blogapp/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello world このページは投稿のインデックスです")
+    # return HttpResponse("hello world このページは投稿のインデックスです")
+    return render(request, 'posts/index.html')
 
 # Create your views here.
